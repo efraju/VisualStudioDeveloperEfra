c5ab783520cb82aaccdb82a65d125ba65a85f29a
efraju <efraju.56@gmail.com>
2018-10-27 05:31:50 +0200

    primer commit

---
+++ BusinessLogic.Test/TrackRepositoryMocked.cs
@@ -0,0 +1,75 @@
+﻿using AutoFixture;
+using DataAccess;
+using DataAccess.Repositories;
+using Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Test
+{
+    public class TrackRepositoryMocked
+    {
+
+        private readonly List<Track> _tracks;
+
+        public TrackRepositoryMocked()
+        {
+            _tracks = GetTracks();
+        }
+
+        public IUnitOfWork GetInstance()
+        {
+            var mocked = new Mock<IUnitOfWork>();
+            mocked.Setup(c => c.Tracks)
+                .Returns(GetRepositoryMocked);
+
+            return mocked.Object;
+        }
+
+        private ITrackRepository GetRepositoryMocked()
+        {
+            var trackMocked = new Mock<ITrackRepository>();
+
+            trackMocked.Setup(c => c.GetAll())
+                .Returns(_tracks);
+
+
+            trackMocked.Setup(c => c.GetById(It.IsAny<int>()))
+                .Returns((int id)=>_tracks.FirstOrDefault(x=>x.TrackId==id));
+
+            return trackMocked.Object;
+        }
+
+        private List<Track> GetTracks()
+        {
+            //var fixture = new Fixture();
+            //var tracks = fixture.CreateMany<Track>
+            //    (10).ToList();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    tracks[i].TrackId = i + 1;
+
+            //}
+            //return tracks;
+            return new List<Track>
+            {
+                new Track
+                {
+                    TrackId=1,
+                    Composer="Cibertec1"
+                },
+
+                 new Track
+                {
+                    TrackId=1,
+                    Composer="Cibertec2"
+                }
+            };
+        }
+    }
+}
---
+++ DataAccess.Tests/BusinessLogic/TrackRepositoryMocked.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using BusinessLogic;
+using DataAccess;
+using DataAccess.Repositories;
+using Models;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary1.BusinessLogic
+{
+    public class TrackRepositoryMocked
+    {
+
+        private readonly List<Track> _tracks;
+
+        public TrackRepositoryMocked()
+        {
+            _tracks = GetTracks();
+        }
+
+        public IUnitOfWork GetInstance()
+        {
+            var mocked = new Mock<IUnitOfWork>();
+            mocked.Setup(c => c.Tracks)
+                .Returns(GetRepositoryMocked);
+
+            return mocked.Object;
+        }
+
+        private ITrackRepository GetRepositoryMocked()
+        {
+            var trackMocked = new Mock<ITrackRepository>();
+
+            trackMocked.Setup(c => c.GetAll())
+                .Returns(_tracks);
+
+            return trackMocked.Object;
+        }
+
+        private List<Track> GetTracks()
+        {
+            var fixture = new Fixture();
+            var tracks = fixture.CreateMany<Track>
+                (10).ToList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tracks[i].TrackId = i + 1;
+
+            }
+            return tracks;
+        }
+    }
+}
